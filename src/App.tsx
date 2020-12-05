e14015f2b4144b9c46e0fa119283b87766174909
sajid-munawar <sajidmunawar666@gmail.com>
2020-12-06 00:40:00 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import {getQuiz} from "./services/quiz_service"
+import { getQuiz } from "./services/quiz_service"
 
 function App() {
-  let [quiz,setQuiz]=useState([])
-  
-useEffect (()=>{
-   async function getquestion(){
-     const data=await getQuiz(5,"easy")
-    //  console.log(data)
-    setQuiz(data)
-   }
-   getquestion();
-},[])
+
+  let [quiz, setQuiz] = useState([] as any)
+
+  useEffect(() => {
+    async function getquestion() {
+      const data = await getQuiz(5, "easy")
+      //  console.log(data)
+      setQuiz(data)
+    }
+    getquestion();
+  }, [])
+  if (!quiz.length) {
+    return <h1>Loading</h1>
+  }
+  //  val = { quiz[1].options }
+  console.log(quiz)
+  console.log(quiz[1].question)
   return (
     <div className="App">
+      <h3>{quiz[1].question}</h3>
+      <ul className="list">{quiz[1].options.map((value: React.ReactNode, ind: string | number | null | undefined) => {
+        return <li key={ind}>{value}</li>
+      })}</ul>
 
     </div>
   );
