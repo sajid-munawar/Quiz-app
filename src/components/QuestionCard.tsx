33ff8fff524235a408c395e9b2e782a87f3da71f
sajid-munawar <sajidmunawar666@gmail.com>
2020-12-06 21:15:52 +0500

    Update QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,8 +1,12 @@
-import React,{FC} from 'react';
-const QuestionCard:FC<any>= ({question,options})=>{
+import React, { FC } from 'react';
+const QuestionCard: FC<any> = ({ question, options }) => {
     // console.log("question="+question)
     // console.log("oop= "+options)
     return <div>
+        <h2>{question}</h2>
+        <ul>{options.map((value:string,ind:number)=>{
+            return <li key={ind}>{value}</li>
+        })}</ul>
 
     </div>
 }
