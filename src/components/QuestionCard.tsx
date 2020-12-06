48ba21d3b7b498373f7312395c450300ee231550
sajid-munawar <sajidmunawar666@gmail.com>
2020-12-06 22:39:09 +0500

    Update QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,14 +1,21 @@
 import React, { FC } from 'react';
-import {questionPropsType} from "./../types/quiz_type";
+import { questionPropsType } from "./../types/quiz_type";
 
-const QuestionCard: FC<questionPropsType> = ({ question, options }) => {
+const QuestionCard: FC<questionPropsType> = ({ question, options,callback }) => {
     // console.log("question="+question)
     // console.log("oop= "+options)
     return <div>
         <h2>{question}</h2>
-        <ul>{options.map((value:string,ind:number)=>{
-            return <li key={ind}>{value}</li>
-        })}</ul>
+        <form onSubmit={callback}>{options.map((opt: string, ind: number) => {
+            return <div key={ind}> 
+            <label>
+                <input type="radio" name="opt" value={opt} />
+                {opt}
+                </label> 
+                </div>
+        })}
+        <input type="submit" />
+        </form>
 
     </div>
 }
