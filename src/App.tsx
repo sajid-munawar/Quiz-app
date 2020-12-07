f74dbf71bc496c480ed1b07f2caf3ba0767a2985
sajid-munawar <sajidmunawar666@gmail.com>
2020-12-07 08:03:28 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,43 @@ function App() {
 
   let [quiz, setQuiz] = useState<quiz_type[]>([])
   let [currentStep,setCurrentStep] =useState(0)
+  let [score,setScore]=useState(0)
+  let [showResult,setShowResult]=useState(false)
 
   useEffect(() => {
     async function getquestion() {
-      const data:quiz_type[] = await getQuiz(5, "easy")
+      const data:quiz_type[] = await getQuiz(2, "easy")
       //  console.log(data)
       setQuiz(data)
     }
     getquestion();
   }, [])
-  if (!quiz.length) {
-    return <h1>Loading</h1>
-  }
+
   const handleSubmit = ((e:React.FormEvent<EventTarget>,userAns:string)=>{
-    console.log(userAns)
+    const currentQuestion:quiz_type=quiz[currentStep]
+    if (userAns===currentQuestion.correct_answer){
+      setScore(++score)
+    }
+    console.log(userAns + " =userAns" +currentQuestion.correct_answer + " =correct Ans")
+    console.log(score)
     e.preventDefault();
     if (currentStep!==quiz.length-1)
      setCurrentStep(++currentStep)
-    else {
-      setCurrentStep(0)
-    }
+    else 
+     setShowResult(true)
+    
   })
   //  val = { quiz[1].options }
   // console.log(quiz)
   // console.log(quiz[0].options)
+  if (!quiz.length) {
+    return <h1>Loading</h1>
+  }
+  if (showResult) {
+    return <div>
+      <h1>Your Score is {score}</h1>
+    </div>
+  }
   return (
     <div className="App">
       {/* <h4>{quiz[0].options}</h4> */}
